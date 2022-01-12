69603949c631c9859b79d6110cf0e2ea35c3fe0d
JackLania <blackofmania.game@gmail.com>
2022-01-12 11:30:08 +0100

    JL Accidental Punch
    Disable accidental Punch

---
+++ jl_accidental_punch/client.lua
@@ -0,0 +1,13 @@
+--====================================================================================
+-- #Author: Jack Lania @LaniaCorp
+--====================================================================================
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		DisableControlAction(1, 140, true)
+		if not IsPlayerTargettingAnything(PlayerId()) then
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+		end
+	end
+end)
---
+++ jl_accidental_punch/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+version '1.1'
+
+server_scripts {
+}
+
+client_scripts {
+  'client.lua'
+}
